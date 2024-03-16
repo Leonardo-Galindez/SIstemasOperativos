da62bbd13ad323f214944efede6cda1f99730def
Leonardo-Galindez <leonardogalindez2018@gmail.com>
2024-03-16 19:52:58 -0300

    ejercicio 4 terminado

---
+++ TP-N°0/ejercicio4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+main(){
+    char a = 'M';
+    /*
+        Aquí, se está usando el especificador de formato %i
+        en printf para imprimir el valor de a como un número
+        entero.
+    */
+    printf("a = %i \n" , a);
+
+    /*
+        Aquí, se está utilizando el especificador
+        de formato %c en printf para imprimir el
+        valor de a como un carácter. 
+    */
+    printf ("La letra %c \n", a);
+}
