892602d5c4ede411bc89952d95f851a3be62660f
Leonardo-Galindez <leonardogalindez2018@gmail.com>
2024-03-25 13:51:05 -0300

    c

--- TP-N°0/ejercicio12.c
+++ TP-N°0/ejercicio12.c
@@ -1,21 +1,46 @@
 #include <stdio.h>
-#include <stdlib.h>	/* para las funciones system y exit */
+#include <stdlib.h>
 
 int main() {
+    int c;
+    char palabra[] = "lenguaje";
+    int intentos = 6;
+    int letras_acertadas = 0;
 
-	int c;
+    /* Decirle al sistema que el modo input es RAW */
+    system ("/bin/stty raw");
 
-	/* Decirle al sistema que el modo input es RAW */
-	system ("/bin/stty raw");
+    printf("Juego del Ahorcado\n");
+    printf("Adivina la palabra secreta. Ingresa una letra (0 para salir).\n");
 
-	while(1) {
-		printf("\r                                                          ");
+    while (1) {
+        printf("\nIntentos restantes: %d\n", intentos);
 		printf("\r c = %c  ingrese una letra (0 para salir): ", c);
-		c = getchar();
+        c = getchar();
 
-		if (c == '0')
-			break;
-	}
+        if (c == '0') {
+            break;
+        } else {
+            //Verificamos en la palabra
+            int acerto = 0;
+            for (int i = 0; palabra[i] != '0'; i++) {
+                if (palabra[i] == c) {
+                    acerto = 1;
+                    break;
+                }
+            }
+            if (!acerto) {
+                intentos--;
+                if (intentos == 0) {
+                    printf("\r¡Te quedaste sin intentos! La palabra era: %s\n", palabra);
+                    break;
+                }
+            }
+        }
+    }
 
-	system ("/bin/stty sane erase ^H");
+    system("/bin/stty sane erase ^H");
+    printf("\nFin del juego\n");
+
+    return 0;
 }
