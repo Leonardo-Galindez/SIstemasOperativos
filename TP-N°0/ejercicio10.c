04d3eb6baba70ace51a0b37b8bb4ba2ec3ab5887
Leonardo-Galindez <leonardogalindez2018@gmail.com>
2024-03-25 12:44:09 -0300

    cambios

--- TP-N°0/ejercicio10.c
+++ TP-N°0/ejercicio10.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
 
 void main(){
-    /*a y b tienen que ser float para no utilizar la division entera*/
-    /*float a, b;
-    float r;
-    a = 5;
+    int a, b, c, d;
+    a = 1;
     b = 2;
-    r = a / b;	
-    printf("%f",r);*/
-    printf("%d\n", 20/3);
-    printf("%f\n", 20/3);
-    printf("%f\n", 20/3.);
-    printf("%d\n", 10%3); 
-    printf("%d\n", 3.1416);
-    printf("%f\n", (double)20/3); 
-    printf("%f\n", (int)3.1416);
-    printf("%d\n", (int)3.1416);
-
+    c = a / b; 
+    d = a / c; 
 }
