e5a04d6ccbde0bcae8e7df86aeb245b79d7ef4a1
Leonardo-Galindez <leonardogalindez2018@gmail.com>
2024-03-28 15:52:25 -0300

    cambios

--- TP-N°0/ejercicio12.c
+++ TP-N°0/ejercicio12.c
@@ -3,24 +3,26 @@
 #include <string.h>
 
 int main() {
+    //tengo 2 arreglos , la copiaPalabra la voy modificando en la ejecucion y palabra no
     char palabra[] = "lenguaje";
     char copiaPalabra[] = "lenguaje";
     int aciertos = 0;
     int intentos = 6;
 	int c;
    
-	/* Decirle al sistema que el modo input es RAW */
+	/* Decirle al sistema que el modo input es RAW (crudo) sin enter*/
 	system ("/bin/stty raw");
     /* Sirve para saltar a la linea de abajo en el principio*/
 	while(1) {
-
+        /*Mostramos la palabra oculta*/
         printf("\rPalabra: ");
         /*srtlen sirve para saber la longitud de una cadena de caracteres sin el 0*/
-        for (int i = 0; i<=strlen(palabra); i++) {
+        for (int i = 0; i<strlen(palabra); i++) {
+            //este for va mostrardo si acertamos una
             if (copiaPalabra[i] == c) {
                 printf("%c ", c);
                 aciertos ++;
-                copiaPalabra[i] = '-';
+                copiaPalabra[i] = '-';//marcamos los aciertos
             } else {
                 printf("_ ");
             }
@@ -35,27 +37,28 @@ int main() {
             break;
         }else{
             int acerto = 0;
-            for(int i=0; i<=strlen(palabra) ;i++){
+            //verificamos si esta la letra en la cadena
+            for(int i=0; i<strlen(palabra) ;i++){
                 if(copiaPalabra[i] == c){
                     acerto = 1;
                     break;
                 }
             }
-
+            //si no esta descontamos los intentos
             if(!acerto){
                 intentos--;
                 if(intentos == 0){
-                    printf("PERDISTE La palabra era: %s", palabra);
+                    printf("\rPERDISTE La palabra era: %s", palabra);
                     break;
                 }
             }
         }
-        if (aciertos == strlen(palabra)) {
+        if (aciertos == strlen(palabra)-1) {
             printf("\rGANASTE\r");
             break;
         }
 	}
-	system ("/bin/stty sane erase ^H");/*NO SE RECETEA*/
+	system ("/bin/stty sane erase ^H");//vuelve a estado "sano"
     printf("\nFin del juego\n");
     
 }
--- TP-N°0/ejercicio9.c
+++ TP-N°0/ejercicio9.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-int main() {
-    char variable_char = 0x7F;
-    unsigned char variable_unsigned_char = 0xffff;
+void main() {
+    char variable_char = 0x7F;//[-128,127]
+    unsigned char variable_unsigned_char = 255;//[0,255] 65535 = 255 o 0xfffff
 
     printf("Valor de variable_char antes del incremento: %d \n", variable_char);
     printf("Valor de variable_unsigned_char antes del incremento: %u \n", variable_unsigned_char);
@@ -13,5 +13,4 @@ int main() {
     printf("Valor de variable_char después del incremento: %d\n", variable_char);
     printf("Valor de variable_unsigned_char después del incremento: %u\n", variable_unsigned_char);
 
-    return 0;
 }
