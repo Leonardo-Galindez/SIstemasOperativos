9a7b65185a223d16878b71798c845756c6835054
Leonardo-Galindez <leonardogalindez2018@gmail.com>
2024-04-09 20:31:27 -0300

    cx

---
+++ TP-N°1/ejercicio7.c
@@ -0,0 +1,24 @@
+/* Creación y finalización de proceso en XINU */
+#include <stdio.h>
+void imprimir(void);
+void main(void){
+    int pid , i;
+    
+    pid = create(imprimir, 2048, 20, "process 1", 0);
+    resume(pid);
+    for (i = 0 ; i<=1000 ; i++){
+        putc(CONSOLE,i);
+    }
+    exit();
+    sleep(5);
+    kill(pid);
+    printf("Mate a mi propio hijo (suena horrible)\n");
+}
+/* proceso sndA */
+void imprimir(void){
+    int i;
+    for (i = 1000 ; i<=5000 ; i++){
+        putc(CONSOLE,i);
+    }
+    exit();
+}
